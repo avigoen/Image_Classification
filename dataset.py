89036f4714e4abec19fc90ed76132f3bf7a9f363
Avinash Goen <agoen97@gmail.com>
2023-05-16 11:15:32 +0530

    Dataset Normalization Update

--- dataset.py
+++ dataset.py
@@ -4,19 +4,28 @@ from keras.datasets import cifar10
 from sklearn.decomposition import PCA
 
 
-
 def get_raw():
     (x_train, y_train), (x_test, y_test) = cifar10.load_data()
-    return (x_train, y_train.ravel()), (x_test, y_test.ravel())
+    return (x_train.astype("float"), y_train.ravel().astype("int")), (x_test.astype("float"), y_test.ravel().astype("int"))
 
 def get_2D_normalised():
     (x_train, y_train), (x_test, y_test) = get_raw()
+    
     x_train = x_train.reshape(x_train.shape[0], -1) / 255.0
     x_test = x_test.reshape(x_test.shape[0], -1) / 255.0
+    
+    # Calculate the mean and standard deviation of the pixel values over the training dataset
+    mean = np.mean(x_train, axis=(0,1))
+    std = np.std(x_train, axis=(0,1))
+    
+    # Normalize the pixel values by subtracting the mean and dividing by the standard deviation
+    x_train = (x_train - mean) / std
+    x_test = (x_test - mean) / std
+    
     return (x_train, y_train), (x_test, y_test)
 
 def test_dimensionality_reduction(components):
-    (x_train, y_train), (x_test, y_test) = get_2D_normalised()
+    (x_train, _), _ = get_2D_normalised()
     
     pca = PCA(n_components = components)
     # Fit the train data to the PCA and transform the train set data
